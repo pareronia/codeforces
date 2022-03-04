ae777bc148153d5be8926e794a35567509045219
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-04 23:43:00 +0100

    Round 1646 Problem C

---
+++ src/main/java/com/github/pareronia/codeforces/_1646/c/Main.java
@@ -0,0 +1,160 @@
+package com.github.pareronia.codeforces._1646.c;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.BitSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * C. Factorials and Powers of Two
+ * @see <a href="https://codeforces.com/contest/1646/problem/C">https://codeforces.com/contest/1646/problem/C</a>
+ */
+public class Main {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Main(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private int powers2(final long n) {
+        return BitSet.valueOf(new long[] { n }).cardinality();
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        final long[] f = new long[17];
+        f[0] = 1L;
+        for (int j = 1; j <= 16; j++) {
+            f[j] = j * f[j - 1];
+        }
+        final long n = sc.nextLong();
+        int ans = powers2(n);
+        for (long j = 0; j < (1 << 16); j++) {
+            final BitSet mask = BitSet.valueOf(new long[] { j });
+            final long sum = mask.stream().mapToLong(t -> f[t + 1]).sum();
+            if (sum <= n) {
+                final long rem = n - sum;
+                if (mask.get(0) && (rem & 1) == 1 || mask.get(1) && (rem & 2) == 1) {
+                    continue;
+                }
+                ans = Math.min(ans, powers2(rem) + mask.cardinality());
+            }
+        }
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = sc.nextInt();
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Main.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Main(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Main.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("codeforces"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
